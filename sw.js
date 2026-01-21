8f8bcec6e04b5e4289592214882219a1b5f9a1c0
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-01-21 19:50:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
-const CACHE_NAME = 'escala-pro-v1';
-const assets = ['./'];
+const CACHE_NAME = 'bbraun-escala-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://i.imgur.com/WvMInJv.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&family=JetBrains+Mono:wght@700&display=swap'
+];
 
-self.addEventListener('install', event => {
+// Instalação: Salva os arquivos essenciais no cache
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Ativação: Limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Busca: Serve os arquivos do cache se estiver offline
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
